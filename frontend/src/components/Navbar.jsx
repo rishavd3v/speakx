0cd6d06f8d0ae62da899756270a622b7dc0066f5
Rishav <rishavraj4273@gmail.com>
2025-01-27 15:50:50 +0530

    refractor

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-export default function Navbar(){
+export default function Navbar({handleThemeChange}){
     return(
         <div className="flex justify-center lg:justify-between items-center bg-black text-white p-6 font-bold mb-10 text-lg">
             <div>SPEAK<span className="text-orange-400">X</span></div>
+            <button onClick={handleThemeChange} className="mb-4 p-2 border rounded">
+                Toggle Dark Mode
+            </button>
         </div>
     )
 }
