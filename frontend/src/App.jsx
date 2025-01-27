5a5c94f3a4d4aecc78206a8391a335a8c1245d69
Rishav <rishavraj4273@gmail.com>
2025-01-27 15:44:21 +0530

    feat: loading animation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import QuestionContainer from "./components/QuestionContainer";
 import Pagination from "./components/Pagination";
 import axios from "axios";
 import Navbar from "./components/Navbar";
+import Loading from "./components/Loading";
 
 function App() {
   const [questions, setQuestions] = useState();
@@ -12,17 +13,23 @@ function App() {
   const [type, setType] = useState("");
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const handlePageChange = (newPage) => {
     setPage(newPage);
   };
 
   useEffect(() => {
-    handleSearch();
+    setLoading(true);
+    setQuestions();
     setPage(1);
+    setTotalPages(0);
+    handleSearch();
   }, [type, query]);
 
   useEffect(() => {
+    setLoading(true);
+    setQuestions();
     handleSearch();
   }, [page]);
 
@@ -30,6 +37,7 @@ function App() {
     if (e) e.preventDefault();
     const data = await axios.get(
       "https://speakx-backend.vercel.app/api/questions",
+      // "http://localhost:3000/api/questions",
       {
         params: {
           query,
@@ -41,20 +49,25 @@ function App() {
 
     setQuestions(data.data);
     setTotalPages(Math.ceil(data.data.total_count / 10));
+    setLoading(false);
   }
 
   return (
     <div className="bg-white min-h-screen">
-
       <Navbar />
 
       <div className="md:p-10 p-10">
-
         <div className="md:px-30">
           <Form setQuery={setQuery} setType={setType} />
         </div>
 
-        <QuestionContainer questions={questions} />
+        {loading ? (
+          <Loading />
+        ) : questions && questions.total_count > 0 ? (
+          <QuestionContainer questions={questions} />
+        ) : (
+          <span className="mt-20 flex justify-center">No Results!!!</span>
+        )}
 
         {totalPages >= 1 && (
           <Pagination
@@ -63,9 +76,7 @@ function App() {
             onPageChange={handlePageChange}
           />
         )}
-
       </div>
-
     </div>
   );
 }
---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading(){
+    return(
+        <div className="flex justify-center items-center mt-30">
+            <div className="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+    )
+}
